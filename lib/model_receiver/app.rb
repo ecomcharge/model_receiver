20c89ef4abc3ab33dc8351bb0cf204c4ae3fc8eb
MihailDV <mihaildv@gmail.com>
2013-11-01 14:18:08 +0300

    fix for work with Rails

--- lib/model_receiver/app.rb
+++ lib/model_receiver/app.rb
@@ -3,12 +3,15 @@ require 'rack/contrib/post_body_content_type_parser'
 
 class ModelReceiver
   class App < Sinatra::Base
-    use Rack::PostBodyContentTypeParser
+    use Rack::PostBodyContentTypeParser unless defined?(Rails)
 
     post '/' do
       headers('Content-Type' => 'text/plain')
 
-      model = ModelReceiver.new(params)
+      args = params
+      args = env['action_dispatch.request.request_parameters'] if defined?(Rails)
+
+      model = ModelReceiver.new(args)
       model.modify
 
       if model.has_error?
--- model_receiver.gemspec
+++ model_receiver.gemspec
@@ -17,7 +17,7 @@ Gem::Specification.new do |s|
   s.executables   = `git ls-files -- bin/*`.split("\n").map{ |f| File.basename(f) }
   s.require_paths = ["lib"]
 
-  s.add_runtime_dependency(%q<sinatra>, ["~> 1.4.0"])
+  s.add_runtime_dependency(%q<sinatra>, [">= 1.3"])
   s.add_runtime_dependency(%q<rack-contrib>, [">= 0"])
   s.add_runtime_dependency(%q<activesupport>, [">=0"])
   s.add_development_dependency(%q<rspec>, [">= 2.14.1"])
